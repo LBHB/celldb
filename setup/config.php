c46ed3956db5df237d6536091f4ed681f0f5756c
Stephen David <stephen.v.david@gmail.com>
2013-03-02 04:53:37 -0800

    Sample config.php consistent with instructions in README

--- setup/config.php
+++ setup/config.php
@@ -9,9 +9,18 @@ created 2005-10-10 - SVD - sprouted out of celldb.php
 
   // define constants for connecting to the database
   $dbserver="localhost";
-  $dbuser="david";
-  $dbpassword="nine1997";
+  $dbuser="celldb_user";
+  $dbpassword="celldb_123";
   $dbname="cell";
+  
+  // lab says which group of users / animals should be shown automatically
+  $LAB="mylabname";
+  
+  // display units for animal weights
+  $weightunits="g";     // little animals
+  //$weightunits="kg";  // big animals
+
+  // SETTINGS BELOW ARE DEPRCATED OR UNDER DEVELOPMENT. CHANGE AT YOUR OWN RISK!
   
   // if $ypaccounts is set to 1, then check passwords using "ypmatch"
   // otherwise, store passwords in the database and accounts need not 
@@ -23,12 +32,6 @@ created 2005-10-10 - SVD - sprouted out of celldb.php
   $view="ear";  // use this for auditory data (NOT ACTIVE, use "")
   //$view="eye";  // use this for visual data
 
-  // lab says which group of users / animals should be shown automatically
-  $LAB="lbhb";
-  
-  // display units for animal weights
-  $weightunits="g";     // little animals
-  //$weightunits="kg";  // big animals
 
   //version 0.3: added multi-unit per recording site support!
   // version 0.4, moved over to NSL, own user management system
